033b2d37c90fba559cbdac5eaa1e73335cfe4c73
Dustinouwu <brago22@hotmail.it>
2024-03-07 19:13:51 -0500

    History Details 1/2

--- lib/src/pages/history.dart
+++ lib/src/pages/history.dart
@@ -34,7 +34,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
                     dynamic responseData = snapshot.data;
                     List<dynamic> transactions =
                         responseData['detail']['transactionsData'];
-        
+
                     return SizedBox(
                       // Envuelve el ListView con un Container
                       height: MediaQuery.of(context).size.height,
@@ -43,16 +43,53 @@ class _HistoryScreenState extends State<HistoryScreen> {
                         itemBuilder: (BuildContext context, int index) {
                           var transaction = transactions[index];
                           return ListTile(
-                            title: Text(
-                                'Descripción: ${transaction['descripcion']}'),
+                            title: Column(
+                              children: [
+                                Text(
+                                    'Fecha: ${transaction['fecha_transaccion']}'),
+                                Text('Monto: ${transaction['monto']}'),
+                                Text('Banco:${transaction['bancoAdquiriente']}')
+                              ],
+                            ),
+
                             subtitle: Text('Monto: ${transaction['monto']}'),
-                            onTap: (){
-                              
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Scaffold(
+                                  appBar: AppBar(
+                                    title: Text('Detalle de la transacción'),
+                                  ),
+                                  body: Column(
+                                    children: [
+                                      Text('fecha_transaccion: ${transaction['fecha_transaccion']}'),
+                                      Text('descripcion: ${transaction['descripcion']}'),
+                                      Text('monto:${transaction['monto']}'),
+                                      Text('numero_identificacion_cliente: ${transaction['numero_identificacion_cliente']}'),
+                                      Text('numero_telefonico_cliente: ${transaction['numero_telefonico_cliente']}'),
+                                      Text('estado_transaccion:${transaction['estado_transaccion']}'),
+                                      Text('tipo_pago: ${transaction['tipo_pago']}'),
+                                      Text('usuarioCreador: ${transaction['usuarioCreador']}'),
+                                      Text('marcaTarjeta:${transaction['marcaTarjeta']}'),
+                                      Text('bancoAdquiriente: ${transaction['bancoAdquiriente']}'),
+                                      Text('voucher: ${transaction['voucher']}'),
+                                      Text('cuotas:${transaction['cuotas']}'),
+                                      Text('numMesesGracia: ${transaction['numMesesGracia']}'),
+                                      Text('tieneInteres: ${transaction['tieneInteres']}'),
+                                      Text('valorInteres:${transaction['valorInteres']}'),
+                                      Text('numReferencia: ${transaction['numReferencia']}'),
+                                      Text('numLote:${transaction['numLote']}'),
+                                      Text('numAutorizacion: ${transaction['numAutorizacion']}'),
+                                      Text('resEstado: ${transaction['resEstado']}'),
+                                      
+                                    ],
+                                  ),
+                                )),
+                              );
                             },
                             // Agrega aquí más detalles que desees mostrar
                           );
                         },
-                        
                       ),
                     );
                   }
@@ -67,8 +104,6 @@ class _HistoryScreenState extends State<HistoryScreen> {
 
   Future<dynamic> fetchData() async {
     String token = ModalRoute.of(context)!.settings.arguments as String;
-    print('Token recibido: $token');
-    final String _token = 'key= $token';
 
     final Uri url = Uri.parse(
         'https://apipre.pagoplux.com/intv1/integrations/getTransactionsEstablishmentResource');
